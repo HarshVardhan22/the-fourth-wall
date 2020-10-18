6b027f5d8d3b2c3f4dac7e9cb150ede08a0f2a22
Harshvardhan <hsinha31@gmail.com>
2020-10-19 03:55:07 +0530

    Landing Page done

--- src/components/thirdPage/ThirdPage.js
+++ src/components/thirdPage/ThirdPage.js
@@ -7,37 +7,45 @@ import {RiSecurePaymentFill} from "react-icons/ri"
 
 const ThirdPage = () =>{
     return (
-      <div className="container bg">
+      <div className=" bg">
         <div className="heading">
           <h1>Create The Look</h1>
           <div className="baseLine"></div>
         </div>
+
         <div className="main">
-          <h3>Limited Time Offer</h3>
-          <h1>Special Edition</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id
-            nobis in inventore porro impedit neque quia praesentium animi fugiat
-            error, labore velit recusandae pariatur, rerum dolore architecto
-            consequuntur quibusdam.
-          </p>
-          <br />
-          <h4>Buy this at 20% discount, use Harsh20 as coupon code</h4>
+          <div className="subContainer">
+            <h3>Limited Time Offer</h3>
+            <h1>Special Edition</h1>
+            <h5>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              id nobis in inventore porro impedit neque quia praesentium animi
+              fugiat error, labore velit recusandae pariatur, rerum dolore
+              architecto consequuntur quibusdam.
+            </h5>
+            <br />
+            <h4>Buy this at 20% discount, use Harsh20 as coupon code</h4>
 
-          {/* *******user should be able to upload the image or link***** */}
+            {/* *******user should be able to upload the image or link***** */}
 
-          <button>Upload Image</button>
+            <button>Upload Image</button>
+          </div>
         </div>
+
         <div className="below">
           <div className="icons">
-           <i><ImEarth/></i>
+            <i>
+              <ImEarth size={100} />
+            </i>
             <h3>Worlwide Shipping</h3>
             <h5>
               Rest assured, we will deliver your wish right at your doorstep!!{" "}
             </h5>
           </div>
           <div className="icons">
-          <i><RiSecurePaymentFill/></i>
+            <i>
+              <RiSecurePaymentFill size={100} />
+            </i>
             <h3>Best Quality</h3>
             <h5>
               Placeat, ea sunt autem adipisci aliquid facere laboriosam
@@ -45,7 +53,9 @@ const ThirdPage = () =>{
             </h5>
           </div>
           <div className="icons">
-          <i><AiFillTags/></i>
+            <i>
+              <AiFillTags size={100} />
+            </i>
             <h3>Best Offers</h3>
             <h5>
               Lorem ipsum dolor sit amet consectetur, adipisicing elids sjsh
@@ -53,10 +63,13 @@ const ThirdPage = () =>{
             </h5>
           </div>
           <div className="icons">
-          <i><AiOutlineFileDone/></i>
+            <i>
+              <AiOutlineFileDone size={100} />
+            </i>
             <h3>Secure Payments</h3>
             <h5>
-              Placeat, ea sunt autem adipisci aliquid facere laboriosam voluptatibus.
+              Placeat, ea sunt autem adipisci aliquid facere laboriosam
+              voluptatibus.
             </h5>
           </div>
         </div>
